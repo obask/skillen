84324c1978db17a28531a8b8281367a0b4c4ee30
Oleg Bask <obask@icloud.com>
2016-05-17 20:10:22 +0300

    import celos code

---
+++ src/main/resources/render-page.js
@@ -0,0 +1,28 @@
+
+"use strict";
+
+
+// don't use network in test mode
+var ajaxGetJson = function(url0, data, successCallback) {
+    console.log("ajaxGetJson" + url0);
+};
+
+var slotsNum = 42;
+
+// TODO select some slot to check sidebar render
+
+_internalSlotsData = Immutable.fromJS(MOCK_CONFIG);
+_internalSlotsData = _internalSlotsData.set("navigation", Immutable.Map());
+
+console.log("config loaded", _internalSlotsData.toJS());
+var request = { groups: ["Flume", "Parquetify"], zoom: undefined, time: undefined };
+
+var tmp = React.createElement(CelosMainFetch, { url: "/main", request: request });
+var result = ReactDOMServer.renderToString(tmp);
+
+console.log(result);
+
+//console.log(JSON.stringify(_internalSlotsData.toJS()));
+
+//document.getElementById('content').innerHTML = result;
+
